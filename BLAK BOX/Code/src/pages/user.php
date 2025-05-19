af9a1cdd2b1ba9a6cc9b5d02eade77d6a66cc012
BrayanJac <brayanjacome02@gmail.com>
2025-05-18 23:44:50 -0500

    Add profileUser.php.

--- BLAK BOX/Code/src/pages/user.php
+++ BLAK BOX/Code/src/pages/user.php
@@ -29,10 +29,10 @@
     </div>
     <div class="offcanvas-body">
       <ul class="nav flex-column">
-        <li class="nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="user.php">Home</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="productsUser.php">Products</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="cartUser.php">Cart</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="#">Perfil</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="profileUser.php">Profile</a></li>
         <li class="nav-item"><a class="nav-link text-danger" href="#">Logout</a></li>
       </ul>
     </div>
@@ -44,10 +44,10 @@
         <img src="../../Images/Logoblanco-removebg-preview.png" alt="Blak Box Logo" class="img-fluid" style="max-height: 150px; filter: invert(1) brightness(2);">
       </div>
       <ul class="nav flex-column">
-        <li class="nav-item"><a class="nav-link text-white" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="user.php">Home</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="productsUser.php">Products</a></li>
         <li class="nav-item"><a class="nav-link text-white" href="cartUser.php">Cart</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="#">Perfil</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="profileUser.php">Profile</a></li>
         <li class="nav-item"><a class="nav-link text-danger" href="#">Logout</a></li>
       </ul>
     </aside>
@@ -78,7 +78,7 @@
           <div class="col-md-4">
             <div class="card bg-purple text-white h-100">
               <div class="card-body">
-                <h5 class="card-title text-accent">Perfil</h5>
+                <h5 class="card-title text-accent">Profile</h5>
                 <p class="card-text">Manage your personal information and view your favorite products.</p>
               </div>
             </div>
